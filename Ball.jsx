54a2a16beacc28d7129e6fcab0fd1387ad846356
juyi212 <dea8307@naver.com>
2022-02-20 22:55:09 +0900

    Lotto Games

---
+++ Ball.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const { useState, useRef } = React;
+
+
+const Ball = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+module.exports = Ball;
---
+++ Lotto.jsx
@@ -0,0 +1,17 @@
+const React = require('react')
+const { useState, useRef } = React;
+
+function getWinNumbers() {
+    const candidate = 
+}
+
+
+const Lotto = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+module.exports = Lotto;
